dd30e51643587bd18727e8bf14ed13d999eb591a
Jakov <j.jakovu@gmail.com>
2022-09-30 20:59:10 +0300

    Add project files.

---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,94 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        //[TestCase("filewithbadextension.SLF", true)]
+        //[TestCase("filewithbadextension.slf", true)]
+        //[TestCase("filewithbadextension.foo", false)]
+        //public void IsValidLogFileName_VariousExtensions_ChecksThem(string file, bool expected)
+        //{
+        //    LogAnalyzer analyzer = new LogAnalyzer();
+
+        //    bool result = analyzer.IsValidLogFileName(file);
+
+        //    Assert.AreEqual(expected, result);
+        //}
+
+        private LogAnalyzer m_analyzer = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            m_analyzer = new LogAnalyzer();
+        }
+
+        [Test]
+        public void IsValidFileName_validFileLowerCased_ReturnsTrue()
+        {
+            bool result = m_analyzer
+                .IsValidLogFileName("whatever.slf");
+
+            Assert.IsTrue(result, "имя файла должно быть правильным!");
+        }
+
+        [Test]
+        [Ignore("в этом тесте имеется ошибка")]
+        
+        public void IsValidFileName_ValidFile_ReturnsTrue()
+        {
+
+        }
+
+        [Test]
+        [Category("Быстрые тесты")]
+        public void IsValidFileName_validFileUpperCased_ReturnsTrue()
+        {
+            bool result = m_analyzer
+                .IsValidLogFileName("whatever.SLF");
+
+            Assert.IsTrue(result, "имя файла должно быть правильным!");
+        }
+
+        //[TearDown]
+        //public void TearDown()
+        //{
+        //    // Следующая строка включена для демонстрации антипаттерна.
+        //    // На самом деле, она не нужна. Не поступайте так.
+        //    m_analyzer = null;
+        //}
+
+        [Test]
+        public void IsValidFileName_EmptyFileName_Throws()
+        {
+            LogAnalyzer la = MakeAnalyzer();
+
+            //var ex = Assert.Catch<Exception>(() => la.IsValidLogFileName(""));
+
+            //StringAssert.Contains("имя файла должно быть задано", ex.Message);
+
+            var ex = Assert.Catch<ArgumentException>(() => la.IsValidLogFileName(""));
+
+            Assert.That(ex.Message, Does.Contain("имя файла должно быть задано"));
+        }
+
+        [TestCase("badfile.foo", false)]
+        [TestCase("goodfile.slf", true)]
+        public void IsValidFileName_WhenCalled_ChangesWasLastFileNameValid(string file, bool expected)
+        {
+            LogAnalyzer la = MakeAnalyzer();
+
+            la.IsValidLogFileName(file);
+
+            Assert.AreEqual(expected, la.WasLastFileNameValid);     // Утверждение о состоянии системы
+        }
+
+        private LogAnalyzer MakeAnalyzer()
+        {
+            return new LogAnalyzer();
+        }
+    }
+}
---
+++ LogAn.UnitTests/MemCalculatorTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    internal class MemCalculatorTest
+    {
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            MemCalculator calc = new MemCalculator();
+
+            int lastSum = calc.Sum();
+
+            Assert.AreEqual(0, lastSum);
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangesSum()
+        {
+            MemCalculator calc = MakeCalc();
+
+            calc.Add(1);
+
+            int sum = calc.Sum();
+
+            Assert.AreEqual(1, sum);
+        }
+
+        private static MemCalculator MakeCalc()
+        {
+            return new MemCalculator();
+        }
+    }
+}
